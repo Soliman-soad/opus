3412ad90d852ed42700ae48bcbcb72a85616ff8c
Soliman-soad <mdsolimansoad@gmail.com>
2022-12-20 15:29:07 +0600

    navbar created

---
+++ src/components/Navber/Navber.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Navber = () => {
+    return (
+        <div className="navbar mt-4 bg-base-100 font-[500] text-[14px]">
+          <a className="btn btn-ghost normal-case text-xl text-[#1355FF] font-bold md:absolute left-36"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M16.712 4.33a9.027 9.027 0 011.652 1.306c.51.51.944 1.064 1.306 1.652M16.712 4.33l-3.448 4.138m3.448-4.138a9.014 9.014 0 00-9.424 0M19.67 7.288l-4.138 3.448m4.138-3.448a9.014 9.014 0 010 9.424m-4.138-5.976a3.736 3.736 0 00-.88-1.388 3.737 3.737 0 00-1.388-.88m2.268 2.268a3.765 3.765 0 010 2.528m-2.268-4.796a3.765 3.765 0 00-2.528 0m4.796 4.796c-.181.506-.475.982-.88 1.388a3.736 3.736 0 01-1.388.88m2.268-2.268l4.138 3.448m0 0a9.027 9.027 0 01-1.306 1.652c-.51.51-1.064.944-1.652 1.306m0 0l-3.448-4.138m3.448 4.138a9.014 9.014 0 01-9.424 0m5.976-4.138a3.765 3.765 0 01-2.528 0m0 0a3.736 3.736 0 01-1.388-.88 3.737 3.737 0 01-.88-1.388m2.268 2.268L7.288 19.67m0 0a9.024 9.024 0 01-1.652-1.306 9.027 9.027 0 01-1.306-1.652m0 0l4.138-3.448M4.33 16.712a9.014 9.014 0 010-9.424m4.138 5.976a3.765 3.765 0 010-2.528m0 0c.181-.506.475-.982.88-1.388a3.736 3.736 0 011.388-.88m-2.268 2.268L4.33 7.288m6.406 1.18L7.288 4.33m0 0a9.024 9.024 0 00-1.652 1.306A9.025 9.025 0 004.33 7.288" />
+</svg>
+<span className='text-[#061237]'>Opus</span>
+</a>
+  <div className="navbar-end">
+    <div className="dropdown">
+      <label tabIndex={0} className="btn btn-ghost lg:hidden ">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-32">
+        <li><a>Overview</a></li>
+        <li><a>Pages</a></li>        
+        <li><a>Template</a></li>
+        <li><a>Contact Us</a></li>
+      </ul>
+    </div>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+    <li><a>Overview</a></li>
+        <li><a>Pages</a></li>        
+        <li><a>Template</a></li>
+        
+    </ul>
+  </div>
+  <div className="navbar-end hidden lg:flex md:relative right-36">
+    <a className="btn text-white rounded-sm bg-[#1355FF] border-none">Contact Us</a>
+  </div>
+</div>
+    );
+};
+
+export default Navber;
