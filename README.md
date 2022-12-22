59a65dae54fdd65ad06be11afbc0c57575e7cdb8
Soliman-soad <mdsolimansoad@gmail.com>
2022-12-22 16:02:46 +0600

    deploy done

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 * The  task is done by React js. 
 * Tailwind css is use for styling. 
 * react-aos package also use for animation. 
-* React video has been use for playing video.
+* React player has been use for playing video.
--- src/components/HomeComponent/Video.jsx
+++ src/components/HomeComponent/Video.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Player } from 'video-react';
-
+import post from '../../img/resource2.png'
+import product from '../../img/production.mp4'
+import ReactPlayer from 'react-player/lazy'
 
 const Video = () => {
+    const videoSrc= product
     return (
         <div data-aos="fade-down" className='lg:mx-64 relative mx-4'>
             <div>
@@ -11,12 +14,7 @@ const Video = () => {
                 <div className='w-[64px] h-[64px] bg-[#F2F3F8] absolute -left-5 -top-8 -z-10'></div>
             </div>
             <div className=' mx-auto'>
-            <Player
-            
-      playsInline
-      poster="/assets/poster.png"
-      src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-    />
+            <ReactPlayer url='https://youtu.be/Tn6-PIqc4UM' width playIcon playsinline={true}/>
             </div>
             <div className='w-[450px] h-[108px] lg:absolute right-0 mt-10 lg:mt-0'><p className='text-[18px] font-[400px] text-[#37456C]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget consectetur eros. Aliquam erat volutpat. Praesent pulvinar arcu non lectus dapibus, a pulvinar mauris aliquam.</p></div>
             <div className='w-[92px] hidden lg:block h-[92px] bg-[#F2F3F8] absolute -left-24'></div>
